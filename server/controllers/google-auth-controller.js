37bd577f5b19a36ee01e85fbde911749689ba6ae
Alena Budzko <budzko.alena@gmail.com>
2019-05-20 10:23:05 -0400

    Alenabudzko (#15)
    * Install axios in package.json as dependencies
    
    * Implement google oauth via new controller and new routes for auth in server.js

---
+++ server/controllers/google-auth-controller.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const authController = {}
+const clientID = '367617815829-730pkn7dkfaupsji6eon9b33vhpc8gru.apps.googleusercontent.com';
+const clientSecret = 'Zerdhm_ou81oh_aUDYAoaNBV';
+
+authController.getCode = (req, res, next) => {
+    axios.get('https://accounts.google.com/o/oauth2/v2/auth?client_id=367617815829-730pkn7dkfaupsji6eon9b33vhpc8gru.apps.googleusercontent.com&response_type=code&scope=openid%20email&redirect_uri=http://localhost:3000/homepage')
+    .then((response) => {
+        res.send(response.data);
+    })
+    .catch((error) => {
+        console.log('Error in getting the code:', error);
+    });
+}
+
+authController.getToken = (req, res, next) => {
+    const code = req.query.code;
+   // const sessionState = req.query.session_state;
+    axios.post(`https://www.googleapis.com/oauth2/v4/token?code=${code}&client_id=367617815829-730pkn7dkfaupsji6eon9b33vhpc8gru.apps.googleusercontent.com&client_secret=Zerdhm_ou81oh_aUDYAoaNBV&redirect_uri=http://localhost:3000/homepage&grant_type=authorization_code&Content-Type=application/x-www-form-urlencoded`)
+    .then(response => {
+        let jwt = response.data.id_token;
+        jwt = jwt.split('.')[1];
+        const base64 = Buffer.from(jwt, 'base64').toString();
+        const email = JSON.parse(base64).email;
+        console.log(email);
+        res.cookie('jwt', jwt, { expires: new Date(Date.now() + 900000) })
+    })
+    .catch((err) => {console.log('The error in getting the Token', err)})
+}
+
+module.exports = authController;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const projectController = require('./controllers/projectController.js');
 const db = require('./database');
+const authController = require('./controllers/google-auth-controller');
 
 // Example query to show database is connected
 // db.any(`SELECT * FROM nodes`).then(data => console.log(data));
@@ -14,6 +15,12 @@ app.use(bodyParser.json());
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+//app.get('/github-auth', projectController.createToken);
+
+app.get('/google-init', authController.getCode);
+
+app.get('/homepage', authController.getToken)
 
 app.post('/newproject', projectController.newProject);
 
