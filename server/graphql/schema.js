d84689ed1b1b4aa0f2949f4bb68994f7316c8266
letnewKid <tevinmcquilkin@gmail.com>
2019-05-22 11:24:58 -0400

    created  node schema and the query to find a node

---
+++ server/graphql/schema.js
@@ -0,0 +1,60 @@
+/* eslint-disable prettier/prettier */
+// this will be the request
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = require('graphql');
+const db = require('../database');
+
+const NodeType = new GraphQLObjectType({
+  name: 'Node',
+  fields: () => ({
+    nodeId: { type: GraphQLInt },
+  }),
+});
+const NodeQuery = new GraphQLObjectType({
+  name: 'NodeQuery',
+  fields: {
+    node: {
+      type: NodeType,
+      args: {id: { type: GraphQLInt },},
+      resolve(parent, args) {
+        return db
+          .query(`SELECT * FROM nodes WHERE id=${args.id}`)
+          .then(node => {
+            console.log('I am the query', node[0].id);
+            return {nodeId:node[0].id};
+          });
+      },
+    },
+  },
+});
+
+const NodeMutation = new GraphQLObjectType({
+  name: 'NodeDelete',
+  fields: {
+    node: {
+      type: NodeType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        // this is where I am going to query the database and delete the node from it
+        console.log('I am the parent', parent);
+        return db
+          .query(
+            `DELETE * FROM nodes WHERE id=${args.id} OR parent_id=${args.id}`
+          )
+          .then(res => console.log('deleted nodes from the database', res))
+          .catch(err =>
+            console.error('there was an interal database problem', err)
+          );
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: NodeQuery,
+  mutation: NodeMutation,
+});
